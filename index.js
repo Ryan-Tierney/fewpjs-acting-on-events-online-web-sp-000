941d988384b1cbad788170e9a9c2fc83163f7228
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-19 23:45:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function moveDodgerRight() {
 }
 
 document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
+  if (e.key === "ArrowRight") {
     moveDodgerLeft();
   }
   if (e.key === "ArrowRight") { 
