d082f57550fa8eb6769c7a29a6b7ef086a778cc7
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-19 23:21:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function moveDodgerLeft()
   }
 } 
 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function(e) { 
+  if (e.key === "ArrowRight") { 
+    moveDodgerRight();
+  }
+});
