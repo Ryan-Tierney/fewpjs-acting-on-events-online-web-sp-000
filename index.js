f1bd570d07080fa648558c0607222f5875d7c675
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-19 23:37:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function moveDodgerLeft() {
 function moveDodgerRight() { 
   let RightNumbers = dodger.style.right.replace("px", ""); let right = parseInt(rightNumbers, 10)
   
-  if right > 0) { 
+  if (right > 0) { 
     dodger.style.right = `${right - 1}px`; 
   }
 }
